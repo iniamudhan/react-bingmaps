8d1cc2b4b47f079ff80456a0a8f70c662277fdf8
iniamudhan <inianbaskar@live.com>
2018-03-24 13:16:59 +0530

    Added Readme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
    constructor(props) {
     super(props);
     this.state = {
-        BingMapKey: ""
+        BingMapKey: "YourBingmapsKey"
       }
   }
   render() {
