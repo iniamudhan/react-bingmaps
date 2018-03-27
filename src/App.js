5175e3f51c85aedd586f7bafb3402d1f28ff4327
iniamudhan <inianbaskar@live.com>
2018-03-27 21:48:47 +0530

    App

--- src/App.js
+++ src/App.js
@@ -5,48 +5,80 @@ class App extends Component {
    constructor(props) {
     super(props);
     this.state = {
-        BingMapKey: "YourBingmapsKey"
+        BingMapKey: "YourBingmapsKey",
+        center: [13.0827, 80.2707],
+        zoom: 7,
+        pushPins:[
+              {"location":[13.0827, 80.2707]}
+            ],
+        infoboxes:[
+              {"location":[13.0827, 80.2707], "option":{ title: 'Chennai', description: '...' }, "addHandler": {"type" : "click", callback: this.OnCallBackTest, callbackData: "info"  }},
+            ],
+            infoboxesWithPushPins:[
+              {
+                "location":[13.0827, 80.2707], 
+                "addHandler":"mouseover", 
+                "infoboxOption": { title: 'Chennai', description: '...' },
+                "pushPinOption":{ title: 'Chennai', description: '...' },
+                "infoboxAddHandler": {"type" : "click", callback: this.OnCallBackTest },
+                "pushPinAddHandler": {"type" : "click", callback: this.OnCallBackTest }
+              }
+            ]
       }
   }
-  OnCallBackTest(){
-    alert("OnCallBack");
+  OnCallBackTest(data){
+    alert(data);
+  }
+  btnClick(e){
+    e.preventDefault();
+    this.setState({
+      //center:[19.000000, -96.000000],
+      pushPins: [
+              {"location":[19.000000, -96.000000], "option":{ color: 'red' }, 
+              "addHandler": {"type" : "click", callback: this.OnCallBackTest }},
+               {"location":[13.0827, 80.2707], "option":{ color: 'red' }, 
+              "addHandler": {"type" : "click", callback: this.OnCallBackTest, callbackData: "hello" }}
+            ],
+            zoom:2,
+            infoboxes:[
+              {"location":[13.0827, 70.2707], "option":{ htmlContent:"<div>Custom Area</div>" }}
+            ],
+            infoboxesWithPushPins: [{
+                "location":[13.0827, 70.2707], 
+                "addHandler":"mouseover", 
+                "infoboxOption": { title: 'Chennai', description: '...' },
+                "pushPinOption":{ title: 'Chennai', description: '...' },
+                "infoboxAddHandler": {"type" : "click", callback: this.OnCallBackTest },
+                "pushPinAddHandler": {"type" : "click", callback: this.OnCallBackTest }
+              }]
+    });
   }
   render() {
     var base64Image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAcBAMAAABmCgnjAAAAFVBMVEVHcEz///8aGhpsuy2s4oDRuPMaADiElDMGAAAAAXRSTlMAQObYZgAAAFNJREFUeF7FkLENgDAQA52GGiIxwD8bPBsgBmD/aXCs6JMNcpWtk1wYx8SO6gO2N3kWtDuJBc39UtYTPEc69BIpTVqLgjJOpS7tQ1I5NyiGia2rHyT7OTg7xhBoAAAAAElFTkSuQmCC';
     return (
+      <div>
       <div className="App">
           <ReactBingmaps 
           bingmapKey = {this.state.BingMapKey}
-          center = {[13.0827, 80.2707]}
-          zoom = {7}
+          center = {this.state.center}
+          zoom = {this.state.zoom}
           mapTypeId = {"road"}
           navigationBarMode = {"compact"}
           supportedMapTypes = {["road","canvasDark"]}
           // pushPins = {
-          //   [
-          //     {"location":[13.0827, 80.2707], "option":{ color: 'red' }, "addHandler": {"type" : "click", callback: this.OnCallBackTest }}
-          //   ]
+          //   this.state.pushPins
           // }
           // infoboxes = {
-          //   [
-          //     {"location":[13.0827, 80.2707], "option":{ title: 'Chennai', description: '...' }, "addHandler": {"type" : "click", callback: this.OnCallBackTest }},
-          //     {"location":[13.0827, 80.2707], "option":{ htmlContent:"<div>Custom Area</div>" }}
-          //   ]
+          //   this.state.infoboxes
           // }
-          infoboxesWithPushPins = {[
-            {
-              "location":[49.464210, 8.190396], 
-              "addHandler":"mouseover", 
-              "infoboxOption": { title: 'Chennai', description: '...' },
-              "pushPinOption":{ title: 'Chennai', description: '...' },
-              "infoboxAddHandler": {"type" : "click", callback: this.OnCallBackTest },
-              "pushPinAddHandler": {"type" : "click", callback: this.OnCallBackTest }
-            }
-          ]
-        }
+            infoboxesWithPushPins = {this.state.infoboxesWithPushPins}
           //disableStreetside = {false}
           >
         </ReactBingmaps>
+      </div>
+      <div>
+      </div>
+      <button onClick={this.btnClick.bind(this)}>click</button>
       </div>
     );
   }
